34288014d5e89071dbfaac3895e5458dbdc3cec6
shan <shan@57blocks.com>
2023-03-08 16:41:33 +0800

    add cron job

--- cypress/e2e/mumbai-testnet-token.test.ts
+++ cypress/e2e/mumbai-testnet-token.test.ts
@@ -2,7 +2,14 @@ import { JsonRpcProvider } from '@ethersproject/providers'
 import { ethers, Wallet } from 'ethers'
 
 describe('Mint mumbai test token', () => {
-  beforeEach(() => cy.visit('/'))
+  beforeEach(() =>
+    cy.visit('/', {
+      headers: {
+        'user-agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36',
+      },
+    }),
+  )
 
   it('Mint mumbai testnet token', () => {
     const provider = new JsonRpcProvider(
