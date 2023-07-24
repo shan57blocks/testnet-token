477068eca94151be44c46bbdf34aaba39347310a
shan <shan@57blocks.com>
2023-07-24 14:14:14 +0800

    delete useless code

--- src/index.js
+++ src/index.js
@@ -7,98 +7,95 @@ const delay = require('delay')
 
 puppeteer.use(StealthPlugin())
 
-const getMaticFaucet = async (order) => {
-  const i = order === undefined ? '' : order
-  console.log('Start', i)
-  const provider = new JsonRpcProvider(
-    'https://polygon-mumbai.g.alchemy.com/v2/y0BCzLqxtgsrbWoe2pF_1nvzaYQCylhE',
-    {
-      name: 'Mumbai',
-      chainId: 80001,
-    },
-  )
-  const wallet = ethers.Wallet.createRandom()
-  const { address } = wallet
-  const signer = new Wallet(wallet.privateKey, provider)
-
-  const browser = await puppeteer.launch({
-    headless: false,
-    executablePath: executablePath(),
-  })
-  const page = await browser.newPage()
-  console.log(`address: ${address}`, i)
-  try {
-    await page.goto('https://faucet.polygon.technology/', {
-      waitUntil: 'networkidle0',
-    })
-    const addressInputSelector =
-      '#app > div > div > div.index > div > div > div:nth-child(1) > div > div > div > div:nth-child(6) > div > div.d-flex.flex-column > div > input[type=text]'
-    await page.waitForSelector(addressInputSelector)
-    await page.type(addressInputSelector, address)
-
-    const submitButtonSelector =
-      '#app > div > div > div.index > div > div > div:nth-child(1) > div > div > div > div:nth-child(7) > div > div > button'
-    await page.click(submitButtonSelector)
-
-    const promptSelector =
-      '#app > div > div > div.index > div > div > div:nth-child(1) > div > div.section.position-absolute > div.modal.show > div > div'
-    await page.waitForSelector(promptSelector)
-
-    const promptConfirmBtnSelector =
-      '#app > div > div > div.index > div > div > div:nth-child(1) > div > div.section.position-absolute > div.modal.show > div > div > div:nth-child(2) > div.ps-t-12 > div > button'
-    await page.click(promptConfirmBtnSelector)
-
-    for (let i = 0; i < 20; i++) {
-      await delay(2000)
-      const balance = await provider.getBalance(address)
-      console.log(`Checking token balance ${i}: ${balance.toString()}`)
-      if (balance.gt(0)) {
-        break
-      }
-      if (i === 19) {
-        throw new Error()
-      }
-    }
-    console.log('Sending token', i)
-    const tx = {
-      to: '0x60758B3A6933192D0Ac28Fc1f675364bb4dFAb1d',
-      value: ethers.utils.parseEther('0.199'),
-    }
-    await signer.sendTransaction(tx)
-    console.log('Sent token', i)
-  } catch (error) {
-    console.log(`Failed to get faucet for: ${error}`, i)
-    throw error
-  } finally {
-    console.log(`Mint end`, i)
-    await page.deleteCookie()
-    await page.close()
-  }
-  await browser.close()
-}
-
 const getMaticFaucets = async () => {
-  console.log(`>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>`)
-  for (let i = 0; i < 1000000; i++) {
-    if (i === 0) {
+  let failTimes = 0
+  for (let i = 0; i < 10000; i++) {
+    if (i !== 0) {
+      const tenMinutes = 10 * 60 * 1000
+      const delayTime = tenMinutes * (failTimes + 1)
+      console.log(`Delay time: ${10 * (failTimes + 1)} minutes`)
+      await delay(delayTime)
+    }
+    try {
+      console.log('Start', i)
+      const provider = new JsonRpcProvider(
+        'https://polygon-mumbai.g.alchemy.com/v2/y0BCzLqxtgsrbWoe2pF_1nvzaYQCylhE',
+        {
+          name: 'Mumbai',
+          chainId: 80001,
+        },
+      )
+      const wallet = ethers.Wallet.createRandom()
+      const { address } = wallet
+      const signer = new Wallet(wallet.privateKey, provider)
+
+      const browser = await puppeteer.launch({
+        headless: false,
+        executablePath: executablePath(),
+      })
+      const page = await browser.newPage()
+      console.log(`address: ${address}`, i)
+
       try {
-        getMaticFaucet(i)
+        await page.setUserAgent(
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36',
+        )
+        await page.goto('https://faucet.polygon.technology/', {
+          waitUntil: 'networkidle0',
+        })
+        const addressInputSelector =
+          '#app > div > div > div.index > div > div > div > div > div:nth-child(6) > div > div.d-flex.flex-column > div > input[type=text]'
+        await page.waitForSelector(addressInputSelector)
+        await page.type(addressInputSelector, address)
+
+        await delay(5000)
+
+        const submitButtonSelector =
+          '#app > div > div > div.index > div > div > div > div > div:nth-child(7) > div > div > button'
+        await page.click(submitButtonSelector)
+
+        await delay(5000)
+
+        const promptConfirmBtnSelector =
+          '#app > div > div > div.index > div > div > div.section.position-absolute > div.modal.show > div > div > div:nth-child(2) > div.ps-t-12 > div > button'
+        await page.click(promptConfirmBtnSelector)
+
+        for (let k = 0; k < 30; k++) {
+          await delay(2000)
+          const balance = await provider.getBalance(address)
+          console.log(
+            `Checking token balance ${k}: ${ethers.utils.formatUnits(
+              balance,
+              18,
+            )}`,
+          )
+          if (balance.gt(0)) {
+            console.log('Sending token', i)
+            const tx = {
+              to: '0x60758B3A6933192D0Ac28Fc1f675364bb4dFAb1d',
+              value: ethers.utils.parseEther('0.199'),
+            }
+            await signer.sendTransaction(tx)
+            console.log('Sent token', i)
+            failTimes = 0
+            break
+          }
+          if (k === 29) {
+            failTimes += 1
+          }
+        }
       } catch (error) {
-        console.log(error)
+        console.log(`Failed to get faucet for: ${error}`, i)
+      } finally {
+        console.log(`Mint end`, i)
+        await page.deleteCookie()
+        await page.close()
       }
-    } else {
-      const oneMinute = 60 * 1000
-      const randomDelay = Math.random() * 2 * oneMinute + 5 * oneMinute
-      await delay(randomDelay)
-      try {
-        getMaticFaucet(i)
-      } catch (error) {
-        console.log(error)
-      }
+      await browser.close()
+    } catch (error) {
+      console.log(error)
     }
   }
-  console.log(`<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<`)
 }
 
-// getMaticFaucet()
 getMaticFaucets()
